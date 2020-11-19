ef726daff330f2ba934c1320f749216009c90d17
MrYou12 <74676768+MrYou12@users.noreply.github.com>
2020-11-20 03:17:24 +0700

    Add files via upload
    Pake sh bukan bash
    sh v2.sh

---
+++ v2.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+#Halo Bre:)
+clear
+echo "╔╦═╦╗╔═╗╔╗─╔═╗╔═╗╔═╦═╗╔═╗"
+echo "║║║║║║╦╝║║─║╔╝║║║║║║║║║╦╝"
+echo "║║║║║║╩╗║╚╗║╚╗║║║║║║║║║╩╗"
+echo "╚═╩═╝╚═╝╚═╝╚═╝╚═╝╚╩═╩╝╚═╝"
+echo "===================================="
+echo "Authour : MrYou12"
+echo "Kontak : 089514741440"
+echo "github : https://github.com/MrYou12"
+echo "YouTube : xxxxxx"
+echo "Waktu Membuat : 20-11-2020"
+echo "Jam : 02:56:46"
+echo "===================================="
+echo
+echo "[ Pilih Menu ]"
+echo
+echo "[1] Hack CCTV [✓]"
+echo "[2] Spam Call [✓]"
+echo "[3] Spam Sms [✓]"
+echo "[4] Ratusan Tools Termux [✓]"
+echo "[5] Nik Dan KK Gratis [✓]"
+echo "[6] Informasi Hp [✓]"
+echo "[7] Crack Hash Password [✓]"
+echo "[8] BruteForce [✓]"
+echo "[9] Install Bahan-Bahannya [✓]"
+echo "[10] Coming Soon"
+read -p" [ Hack ]=>" pill
+#Batas Gan
+if [ $pill = "1" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install git
+pkg install python2
+pip2 install requests
+pip2 install request
+git clone https://github.com/storiku/HackCCTV
+cd HackCCTV
+python2 HCCTV.py
+echo "Install Done!"
+fi
+#Batas Gan
+if [ $pill = "2" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install git
+pkg install python
+pip2 install requests
+pip2 install request
+git clone https://github.com/Xractz/SpamCall
+cd SpamCall
+python spam.py
+echo "Install Done!"
+fi
+#Batas Gan
+if [ $pill = "3" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install git
+pkg install python2
+pkg install python
+pip2 install requests
+pip2 install request
+pkg install php
+git clone https://github.com/dz-id/SimpleSpamOtp
+cd SimpleSpamOtp
+php s-SpamOtp.php
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "4" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install git
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "5" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install php git
+git clone https://github.com/IndonesiaSecurity/kkktp
+cd kkktp
+php kkktp.php
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "6" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install neofetch
+neofetch
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "7" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install python2
+pip2 install requests
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "8" ]
+then
+echo "Proses Install..." ;sleep2
+pkg install pip
+pip install wordlist
+pkg install worlist
+wordlist -h
+cd /sdcard
+cat pas.txt
+echo "Proses Done!"
+fi
+#Batas Gan
+if [ $pill = "9" ]
+then
+echo "╔═╗╔═╗╔═╗╔══╗╔═╗╔══╗"
+echo "║╬║║╬║║║║║══╣║╦╝║══╣"
+echo "║╔╝║╗╣║║║╠══║║╩╗╠══║"
+echo "╚╝─╚╩╝╚═╝╚══╝╚═╝╚══╝"
+pkg install git
+pkg install python2
+pkg install bash
+pip2 install requests
+pip2 install request
+echo "Proses Done!"
+fi
+#Batas Gan
